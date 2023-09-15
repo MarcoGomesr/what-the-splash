41aa9bd9b11e7e39c70774d160ddedbaf5e1d1a8
DESKTOP-B1OEVKN\marcogomesr <marcogomesr@gmail.com>
2023-09-15 20:33:44 +0200

    add button component and error

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -2,26 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loadImages } from '../../actions';
 import './styles.css';
-
-const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
+import Button from '../Button';
 
 class ImageGrid extends Component {
-    state = {
-        images: [],
-    };
-
     componentDidMount() {
-        fetch(`https://api.unsplash.com/photos/?client_id=${key}&per_page=28`)
-            .then(res => res.json())
-            .then(images => {
-                this.setState({
-                    images,
-                });
-            });
+        this.props.loadImages();
     }
 
     render() {
-        const { images } = this.state;
+        const { error, isLoading, loadImages } = this.props;
+        const images = [].concat(...this.props.images);
         return (
             <div className="content">
                 <section className="grid">
@@ -39,7 +29,13 @@ class ImageGrid extends Component {
                         </div>
                     ))}
                 </section>
-                <button onClick={this.props.loadImages}>Load More</button>
+                {error && <div className="error">{JSON.stringify(error)}</div>}
+                <Button
+                    onClick={() => !isLoading && loadImages()}
+                    loading={isLoading}
+                >
+                    Load More
+                </Button>
             </div>
         );
     }
